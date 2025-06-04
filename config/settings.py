7d68694162d08715396d52726ec40fe6d4b6f93d
Temirov0324 <temirovelbek2406@gmail.com>
2025-06-04 14:51:13 +0500

    Deploy uchun tayyor2

--- config/settings.py
+++ config/settings.py
@@ -9,9 +9,14 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import os
-from datetime import timedelta
+
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')  # bu aniq yo‘l bilan yuklaydi
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
